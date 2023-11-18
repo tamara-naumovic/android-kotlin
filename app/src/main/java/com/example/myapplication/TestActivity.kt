250621154fd1473b1eb2f42353122b31c740ccc0
Tamara Naumovic <tamaranaumovic@gmail.com>
2023-11-18 14:52:24 +0100

    test lifecycle

--- app/src/main/java/com/example/myapplication/TestActivity.kt
+++ app/src/main/java/com/example/myapplication/TestActivity.kt
@@ -1,26 +1,18 @@
 package com.example.myapplication
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-    val TAG = "Main Activity "
+class TestActivity : AppCompatActivity() {
+    val TAG = "Test Activity "
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_test)
         Log.d(TAG, "onCreate")
 
-
-        val btnNext = findViewById<Button>(R.id.btn_next)
-        btnNext.setOnClickListener{
-            val intent = Intent(this, TestActivity::class.java)
-            startActivity(intent)
-        }
     }
-
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart")
