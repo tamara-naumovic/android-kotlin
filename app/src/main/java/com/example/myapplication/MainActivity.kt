02b34d0229d234afb59504766537217844c624b8
Tamara Naumovic <tamaranaumovic@gmail.com>
2023-12-05 10:08:00 +0100

    todo init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,40 +7,11 @@ import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    val TAG = "Main Activity "
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate")
-
-
-        val btnNext = findViewById<Button>(R.id.btn_next)
-        btnNext.setOnClickListener{
-            val intent = Intent(this, TestActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume")
 
     }
 
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-
-    }
 }
